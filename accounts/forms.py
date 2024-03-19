b43776d1e532b7f98a43226178dddf409f095118
Kerriea-star <ojared511@gmail.com>
2024-03-19 13:47:42 +0300

    decorators, forms, sendmails, tokens

---
+++ accounts/forms.py
@@ -0,0 +1,53 @@
+from django import forms
+from accounts.models import User, Profile
+
+class ProfileForm(forms.ModelForm):
+    
+    full_name = forms.CharField(max_length=50)
+    email = forms.CharField(max_length=50)
+    username = forms.CharField(max_length=50)
+    town = forms.CharField(max_length=50)
+    county = forms.CharField(max_length=50)
+
+    password1 = forms.CharField(
+        required=False, label="Password", widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label="Password Confirmation",required=False,
+        widget=forms.PasswordInput)
+    
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop('user', None)
+        super(ProfileForm, self).__init__(*args, **kwargs)
+        if user:
+            self.fields['phone'].initial = user.phone
+            self.fields['full_name'].initial = user.full_name
+            self.fields['email'].initial = user.email
+            self.fields['username'].initial = user.username
+            self.fields['town'].initial = user.town
+            self.fields['county'].initial = user.county
+        for field_name, field in self.fields.items():
+            self.fields[field_name].widget.attrs.update({'class':'form-control'})
+            # self.fields[field].widget.attrs.update({'style': 'width: 350px;'})
+        # Set email field as read-only
+        self.fields['full_name'].widget.attrs.update({'readonly':True})
+        self.fields['email'].widget.attrs.update({'readonly':True})
+        self.fields['profile_picture'].widget.attrs.update({'class':'account-settings-fileinput'})
+
+    class Meta:
+        model = Profile
+        fields = ['phone','email','password1','password2','full_name','bio','profile_picture', 'username', 'town']
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+
+        print(f"Hello {password2}")
+
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match!")
+
+        return password2
+# def convert_phone_number(phone_number):
+#     parsed_number = phonenumbers.parse(phone_number, "KE")  # Assuming the phone number is from Kenya (change the country code accordingly)
+#     formatted_number = phonenumbers.format_number(parsed_number, phonenumbers.PhoneNumberFormat.INTERNATIONAL)
+#     return formatted_number
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,200 @@
 from django.shortcuts import render
 
-# Create your views here.
+import datetime
+from accounts.decorators import administrator_required, client_required
+from accounts.tokens import account_activation_token 
+from django.contrib.sites.shortcuts import get_current_site  
+from django.utils.encoding import force_bytes, force_str  
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode  
+from django.template.loader import render_to_string 
+from accounts.models import Administrator, Client, User, Psychiatrist
+from accounts.sendMails import  send_activation_email
+from accounts.forms import ProfileForm
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.decorators.cache import never_cache
+from django.views.generic import CreateView
+from django.conf import settings
+from django.core.mail import EmailMessage
+from django.contrib.auth import get_user_model
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView
+
+decorators = [never_cache, login_required, administrator_required]
+
+
+
+def clientRegistration(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        username = request.POST.get("username")
+        full_name = request.POST.get("full_name")
+        password = request.POST.get('password')
+        password2 = request.POST.get('confirm-password')
+
+        if password != password2:
+            messages.error(request,"Password didn't match")
+            return render(request,'accounts/sign_up.html')
+        user = User(email=email, username=username, full_name=full_name)
+        user.set_password(password2)
+        user.is_active = False
+        user.save()
+        send_activation_email(user,request)
+        
+        client = Client(user=user)
+        client.save()
+        messages.success(request,"Account created succesfully")
+        return render(request,'accounts/sign_alert.html')
+    return render(request,'accounts/sign_up.html')
+
+
+def psycRegistration(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        username = request.POST.get("username")
+        full_name = request.POST.get("full_name")
+        kmpdb = request.POST.get("kmpdb")
+        password = request.POST.get('password')
+        password2 = request.POST.get('confirm-password')
+
+        if password != password2:
+            messages.error(request,"Password didn't match")
+            return render(request,'accounts/psycsign_up.html')
+        user = User(email=email, username=username, full_name=full_name)
+        user.set_password(password2)
+        user.role = 'Psychiatrist'
+        user.is_active = False
+        user.save()
+        send_activation_email(user,request)
+        
+        psyco = Psychiatrist(user=user,kmpdb=kmpdb)
+        psyco.save()
+        messages.success(request,"Account created succesfully")
+        context = {
+            'psyco':True,
+        }
+        return render(request,'accounts/sign_alert.html',context)
+    return render(request,'accounts/psycsignup.html')
+
+
+def login_user(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        try:
+            user= User.objects.get(email=email)
+        except User.DoesNotExist:
+            messages.error(request, 'email does not exist!') 
+        user = authenticate(request, email=email, password=password)
+        
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                messages.success(request, 'Logged in succesfully')
+                return redirect('/dashboard/')
+            else:
+                messages.error(request, 'Please activate your account')
+                return redirect('/login/') 
+        else:
+            messages.error(request, 'Incorrect password')
+            return redirect('/accounts/login/')
+    return render(request,'accounts/login.html')
+
+def psyclogin(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        kmpdb = request.POST.get('kmpdb')
+        password = request.POST.get('password')
+        try:
+            psyco= Psychiatrist.objects.get(kmpdb=kmpdb)
+        except Psychiatrist.DoesNotExist:
+            messages.error(request, 'Psychiatrist does not exist!')
+            return redirect('/accounts/psyclogin/') 
+        if psyco.user.email == email:
+            user = authenticate(request, email=email, password=password)
+        
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    messages.success(request, 'Logged in succesfully')
+                    return redirect('/dashboard/')
+                else:
+                    messages.error(request, 'Please activate your account')
+                    return redirect('/accounts/psyclogin/') 
+            else:
+                messages.error(request, 'Incorrect password')
+                return redirect('/accounts/psyclogin/')
+        else:
+            messages.error(request, 'Incorrect email')
+            return redirect('/accounts/psyclogin/')
+    return render(request,'accounts/psyclogin.html')
+
+
+#logout the logged in user   
+def log_out(request):
+    logout(request)
+    return redirect('/')
+
+def RequestPasswordReset(request):
+    context = {
+        # Add context data as needed
+    }
+    return render(request, "accounts/RequestPasswordReset.html", context)
+
+
+
+def activate(request, uidb64, token):  
+    User = get_user_model() 
+    
+    try:  
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid) 
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):  
+        user = None  
+    if user is not None and account_activation_token.check_token(user, token):  
+        user.is_active = True  
+        user.save()
+        login(request,user)  
+        messages.success(request,"Account was Successfully Verified.")
+        return HttpResponse('Thank you for your email confirmation. Now you can login your account.')  
+    else:  
+        return HttpResponse('Activation link is invalid!')
+
+
+def edit_profile(request):
+    r_user = User.objects.get(id=request.user.id)
+    if request.user.role == 'Psychiatrist':
+        user = Psychiatrist.objects.get(user=r_user)
+    else:
+        user = Client.objects.get(user=r_user)
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES, instance=user, user=r_user)
+        if form.is_valid():
+            phone = form.cleaned_data['phone']
+            full_name = form.cleaned_data['full_name']
+            town = form.cleaned_data['town']
+            county = form.cleaned_data['county']
+            username = form.cleaned_data['username']
+            password2 = form.cleaned_data['password2']
+            # image = request.FILES['image']
+            if len(request.FILES) != 0:
+                if len(user.profile_picture) > 0:
+                    user.profile_picture = request.FILES['image']
+            form.save()
+            r_user.phone = phone
+            r_user.full_name = full_name
+            r_user.username = username
+            r_user.town = town
+            r_user.county = county
+            r_user.save()
+            if len(password2) > 0:
+                r_user.set_password(password2)
+            messages.success(request, 'Updated succesfully')
+            return redirect('/dashboard/')
+    else:
+        form = ProfileForm(instance=user, user=r_user)
+
+    return render(request, 'accounts/profile.html', {'form': form,'user':user})
