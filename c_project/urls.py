30c8ea8e6007569c7de28c022197cf7523c8834f
Kerriea-star <ojared511@gmail.com>
2024-03-21 10:00:25 +0300

    fixes

--- c_project/urls.py
+++ c_project/urls.py
@@ -5,7 +5,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
+    path('', include('accounts.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
